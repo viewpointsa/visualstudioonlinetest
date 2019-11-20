bf4ba94191c22519040a8f904070679b38056bbc
Maxime Dauphin <mdauphin@viewpoint.fr>
2019-11-20 14:37:24 +0000

    Some test with libmicrohttpd

--- Makefile
+++ Makefile
@@ -9,12 +9,14 @@ SRC_DIR := ./src
 OBJ_DIR := ./obj
 SRC_FILES := $(wildcard $(SRC_DIR)/*.cpp)
 OBJ_FILES := $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(SRC_FILES))
-CPPFLAGS := -g
+LIBS := libmicrohttpd
+CXXFLAGS := -g `pkg-config --cflags $(LIBS)` -std=c++0x -I/usr/include/flycapture/ -DWITH_GPU -DTRACK -DROTATE
+LDFLAGS := `pkg-config --libs $(LIBS)`
 
 
 $(PROGRAM): $(OBJ_FILES)
 	$(CXX) $(LDFLAGS) -o $@ $^
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(CXXFLAGS) -c -o $@ $<
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include "Test.h"
+#include <microhttpd.h>
+#include <string.h>
 
-int main(int argc, char** argv)
+#define PORT 9080
+
+int answer_to_connection(void *cls, struct MHD_Connection *connection,
+                         const char *url,
+                         const char *method, const char *version,
+                         const char *upload_data,
+                         size_t *upload_data_size, void **con_cls)
 {
-    Test test;
-    test.SetName("MyName");
-    std::cout << test.GetName() << std::endl ;    
+    const char *page = "<html><body>Hello, browser!</body></html>";
+    struct MHD_Response *response;
+    int ret;
+
+    response = MHD_create_response_from_buffer(strlen(page),
+                                               (void *)page, MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+
+    return ret;
+}
+
+int main(int argc, char **argv)
+{
+    struct MHD_Daemon *daemon;
+    daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY|MHD_USE_DEBUG, PORT, NULL, NULL,
+                              &answer_to_connection, NULL, MHD_OPTION_END);
+    if (nullptr == daemon)
+        return 1;
+    printf("running http://localhost:%d/\n", PORT);
+    getchar();
+
+    MHD_stop_daemon(daemon);
     return 0;
 }
